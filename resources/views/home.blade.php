b851e8ded078488610bec187bede7ff00b463ac8
PapiMatteo <matteopapi.work@gmail.com>
2024-01-17 16:20:22 +0100

    add: dta table visualization

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to laravel</h1>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Prezzo</th>
+            <th scope="col">Tempo di Preparazione</th>
+            <th scope="col">Tipologia di Bicchiere</th>
+            <th scope="col">Difficolta Preparazione</th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr>
+                @foreach ($cocktails as $cocktail)
+                    <th scope="row">{{ $cocktail->name }}</th>
+                    <td>{{ $cocktail->price }}</td>
+                    <td>{{ $cocktail->prep_time }}</td>
+                    <td>{{ $cocktail->glass_type }}</td>
+                    <td>{{ $cocktail->prep_difficulty }}</td>
+                @endforeach
+                
+            </tr>
+        </tbody>
+  </table>
 @endsection
