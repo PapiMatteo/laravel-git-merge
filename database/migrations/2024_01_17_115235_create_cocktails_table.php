aa7b083b576bfd3cdbb9e9723a4b198f7883afa5
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2024-01-17 12:57:38 +0100

    add: migration

---
+++ database/migrations/2024_01_17_115235_create_cocktails_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cocktails', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->smallInteger('price');
+            $table->time('prep_time');
+            $table->string('glass_type');
+            $table->string('prep_difficulty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cocktails');
+    }
+};
