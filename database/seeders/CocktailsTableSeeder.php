0ac8d98be84282bd4de47120aa8d545ea2389833
valerioperria <valerio.perria@gmail.com>
2024-01-17 15:53:41 +0100

    fix: main seeder

--- database/seeders/CocktailsTableSeeder.php
+++ database/seeders/CocktailsTableSeeder.php
@@ -20,13 +20,10 @@ class CocktailsTableSeeder extends Seeder
 
 
 
-        $cocktail = new Cocktail();
+
         foreach ($names as $name) {
+            $cocktail = new Cocktail();
             $cocktail->name = $name;
-        }
-
-        for ($i = 0; $i < 4; $i++) {
-
             $cocktail->price = $faker->numberBetween(4, 20);
             $cocktail->prep_time = $faker->time('00:i');
             $cocktail->glass_type = $faker->randomElement($glasses);
@@ -34,40 +31,44 @@ class CocktailsTableSeeder extends Seeder
 
             // dd($cocktail);
             $cocktail->save();
-
-
-
-            // $cocktail = new Cocktail();
-            // $cocktail->name = 'Daiquiri';
-            // $cocktail->price = 10;
-            // $cocktail->prep_time = ;
-            // $cocktail->glass_type = 'Nick & Nora';
-            // $cocktail->prep_difficulty = 'medium';
-            // $cocktail->save();
-
-            // $cocktail = new Cocktail();
-            // $cocktail->name = 'Negroni';
-            // $cocktail->price = 8;
-            // $cocktail->prep_time = ;
-            // $cocktail->glass_type = 'Old Fashioned';
-            // $cocktail->prep_difficulty = 'easy';
-            // $cocktail->save();
-
-            // $cocktail = new Cocktail();
-            // $cocktail->name = 'Mint Julep';
-            // $cocktail->price = 12;
-            // $cocktail->prep_time = ;
-            // $cocktail->glass_type = 'Julep Cup';
-            // $cocktail->prep_difficulty = 'hard';
-            // $cocktail->save();
-
-            // $cocktail = new Cocktail();
-            // $cocktail->name = 'Gin Fizz';
-            // $cocktail->price = 10;
-            // $cocktail->prep_time = ;
-            // $cocktail->glass_type = 'Collins';
-            // $cocktail->prep_difficulty = 'medium';
-            // $cocktail->save();
         }
+
+
+
+
+
+
+        // $cocktail = new Cocktail();
+        // $cocktail->name = 'Daiquiri';
+        // $cocktail->price = 10;
+        // $cocktail->prep_time = ;
+        // $cocktail->glass_type = 'Nick & Nora';
+        // $cocktail->prep_difficulty = 'medium';
+        // $cocktail->save();
+
+        // $cocktail = new Cocktail();
+        // $cocktail->name = 'Negroni';
+        // $cocktail->price = 8;
+        // $cocktail->prep_time = ;
+        // $cocktail->glass_type = 'Old Fashioned';
+        // $cocktail->prep_difficulty = 'easy';
+        // $cocktail->save();
+
+        // $cocktail = new Cocktail();
+        // $cocktail->name = 'Mint Julep';
+        // $cocktail->price = 12;
+        // $cocktail->prep_time = ;
+        // $cocktail->glass_type = 'Julep Cup';
+        // $cocktail->prep_difficulty = 'hard';
+        // $cocktail->save();
+
+        // $cocktail = new Cocktail();
+        // $cocktail->name = 'Gin Fizz';
+        // $cocktail->price = 10;
+        // $cocktail->prep_time = ;
+        // $cocktail->glass_type = 'Collins';
+        // $cocktail->prep_difficulty = 'medium';
+        // $cocktail->save();
+
     }
 }
