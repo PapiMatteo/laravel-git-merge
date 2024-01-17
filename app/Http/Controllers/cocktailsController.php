f46b994594eff94719500f8347315171d6b13ef6
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-01-17 15:16:25 +0100

    add: cocktailsController

--- app/Http/Controllers/cocktailsController.php
+++ app/Http/Controllers/cocktailsController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cocktails;
 use Illuminate\Http\Request;
 
-class cocktailsController extends Controller
+class CocktailController extends Controller
 {
-    
+    public function index()
+    {
+        $cocktails = Cocktails::all();
+        return view('cocktails.index', compact('cocktails'));
+    }
+
+    // Altre azioni come show, create, store, ecc.
 }
