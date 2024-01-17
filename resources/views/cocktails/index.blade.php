94551a97b6533aa42c78fbc5f56b600668763f77
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2024-01-17 17:06:46 +0100

    fix: some errors

--- resources/views/cocktails/index.blade.php
+++ resources/views/cocktails/index.blade.php
@@ -3,25 +3,24 @@
 @section('content')
     <table class="table">
         <thead>
-        <tr>
-            <th scope="col">Nome</th>
-            <th scope="col">Prezzo</th>
-            <th scope="col">Tempo di Preparazione</th>
-            <th scope="col">Tipologia di Bicchiere</th>
-            <th scope="col">Difficolta Preparazione</th>
-        </tr>
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Tempo di Preparazione</th>
+                <th scope="col">Tipologia di Bicchiere</th>
+                <th scope="col">Difficolta Preparazione</th>
+            </tr>
         </thead>
         <tbody>
-            <tr>
-                @foreach ($cocktails as $cocktail)
+            @foreach ($cocktails as $cocktail)
+                <tr>
                     <th scope="row">{{ $cocktail->name }}</th>
                     <td>{{ $cocktail->price }}</td>
                     <td>{{ $cocktail->prep_time }}</td>
                     <td>{{ $cocktail->glass_type }}</td>
                     <td>{{ $cocktail->prep_difficulty }}</td>
-                @endforeach
-                
-            </tr>
+                </tr>
+            @endforeach
         </tbody>
-  </table>
+    </table>
 @endsection
