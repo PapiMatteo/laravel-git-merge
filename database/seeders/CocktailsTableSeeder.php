fff3b0939ab5a86724670566c82f94a80ec15672
valerioperria <valerio.perria@gmail.com>
2024-01-17 15:27:05 +0100

    add: seeder

---
+++ database/seeders/CocktailsTableSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cocktail;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class CocktailsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $names = ['Daiquiri', 'Negroni', 'Gin Fizz', 'Mint Julep'];
+        $glasses = ['Collins', 'Old Fashioned', 'Nick & Nora'];
+        $difficulty = ['hard', 'medium', 'easy'];
+
+        for($i = 0; $i < 4; $i++) {
+
+            $cocktail = new Cocktail();
+            $cocktail->name = $faker->randomElement($names);
+            $cocktail->price = $faker->numberBetween(4, 20);
+            $cocktail->prep_time = $faker->time('00:i');
+            $cocktail->glass_type = $faker->randomElement($glasses);
+            $cocktail->prep_difficulty = $faker->randomElement($difficulty);
+
+            $cocktail->save();
+
+            // $cocktail = new Cocktail();
+            // $cocktail->name = 'Daiquiri';
+            // $cocktail->price = 10;
+            // $cocktail->prep_time = ;
+            // $cocktail->glass_type = 'Nick & Nora';
+            // $cocktail->prep_difficulty = 'medium';
+            // $cocktail->save();
+
+              // $cocktail = new Cocktail();
+            // $cocktail->name = 'Negroni';
+            // $cocktail->price = 8;
+            // $cocktail->prep_time = ;
+            // $cocktail->glass_type = 'Old Fashioned';
+            // $cocktail->prep_difficulty = 'easy';
+            // $cocktail->save();
+
+              // $cocktail = new Cocktail();
+            // $cocktail->name = 'Mint Julep';
+            // $cocktail->price = 12;
+            // $cocktail->prep_time = ;
+            // $cocktail->glass_type = 'Julep Cup';
+            // $cocktail->prep_difficulty = 'hard';
+            // $cocktail->save();
+
+              // $cocktail = new Cocktail();
+            // $cocktail->name = 'Gin Fizz';
+            // $cocktail->price = 10;
+            // $cocktail->prep_time = ;
+            // $cocktail->glass_type = 'Collins';
+            // $cocktail->prep_difficulty = 'medium';
+            // $cocktail->save();
+        }
+    }
+}
