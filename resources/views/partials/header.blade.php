ac760e6bd538c3542ff5cff6bb5fb5f1a418ea55
PapiMatteo <matteopapi.work@gmail.com>
2024-01-17 12:59:30 +0100

    add: header structure

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Our Cocktails</a>
+        </div>
+    </nav>
+      
+
+</header>
