113456912ac72c593a902d913e79e97a13cf86f1
PapiMatteo <matteopapi.work@gmail.com>
2024-01-25 16:38:31 +0100

    add: request WIP

--- app/Http/Requests/StoreCocktailRequest.php
+++ app/Http/Requests/StoreCocktailRequest.php
@@ -11,7 +11,7 @@ class StoreCocktailRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class StoreCocktailRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name'  => ['required', 'min:2', 'max:100'],
+            'price' => ['required'],
+            'ingredient' => ['required', 'min:2', 'max:250'],
+            
         ];
     }
 }
