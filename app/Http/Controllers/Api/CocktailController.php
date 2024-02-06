197cd87c3784412272b8ef9c2c41cdbd014a4994
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2024-02-06 16:01:08 +0100

    add: api call

---
+++ app/Http/Controllers/Api/CocktailController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cocktail;
+use Illuminate\Http\Request;
+
+class CocktailController extends Controller
+{
+    public function index() {
+        $cocktails = Cocktail::with('ingredients')->get();
+        return response()->json([
+            'results' => $cocktails,
+            'success' => true
+        ]);
+    }
+}
