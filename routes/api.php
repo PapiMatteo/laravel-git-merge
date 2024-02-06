a5ade47d1d7faee384798aac12117be6e5754121
PapiMatteo <matteopapi.work@gmail.com>
2024-02-06 16:36:54 +0100

    add: api route for show

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/cocktails', [CocktailController::class, 'index']);
+Route::get('/cocktails/{slug}', [CocktailController::class, 'show']);
